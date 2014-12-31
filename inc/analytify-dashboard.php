6e5a8f5c3ddef066c9e6c06794a1e33d0d600a74
Muhammad Adnan <hiddenpearls@gmail.com>
2015-01-01 02:09:25 +0500

    changed live-stats preview image + pretty links

--- inc/analytify-dashboard.php
+++ inc/analytify-dashboard.php
@@ -60,7 +60,7 @@ if( isset( $ed_date ) ) {
 									</form>
 								</div>
 								
-								<a target="_blank" href="http://wp-analytify.com/?utm_source=analytify-live&utm_medium=wordpressdotorg&utm_term=promotion&utm_content=live-stats-img&utm_campaign=cm-analytify-free" title="Upgrade to PRO to enjoy full features of Analytify.">
+								<a target="_blank" href="http://wp-analytify.com/upgrade-from-free" title="Upgrade to PRO to enjoy full features of Analytify.">
 									<img class="gray-areas" src="<?php echo plugins_url('images/live-stats-preview.png', dirname(__FILE__) );?>" width="100%" height="auto" alt="Upgrade to PRO to enjoy full features of Analytify." />
 								</a>
 
