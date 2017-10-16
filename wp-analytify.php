8f9474603d7020bcd130a4c4b659f3a7b4681538
burhandodhy <burhandodhy@gmail.com>
2017-10-16 09:48:25 +0500

    remove duplication

--- wp-analytify.php
+++ wp-analytify.php
@@ -1024,61 +1024,7 @@ if ( ! class_exists( 'WP_Analytify' ) ) {
 
 			return number_format( $num );
 		}
-
-		/**
-		 * Pretty time to display.
-		 *
-		 * @param int $time time.
-		 */
-		function pa_pretty_time( $time ) {
-
-				// Check if numeric.
-			if ( is_numeric( $time ) ) {
-
-				$value = array(
-					'years'   => '00',
-					'days'    => '00',
-					'hours'   => '',
-					'minutes' => '',
-					'seconds' => '',
-					);
-
-				if ( $time >= 31556926 ) {
-					$value['years'] = floor( $time / 31556926 );
-					$time           = ($time % 31556926);
-				} //$time >= 31556926
-
-				if ( $time >= 86400 ) {
-					$value['days'] = floor( $time / 86400 );
-					$time          = ($time % 86400);
-				} //$time >= 86400
-				if ( $time >= 3600 ) {
-					$value['hours'] = str_pad( floor( $time / 3600 ), 1, 0, STR_PAD_LEFT );
-					$time           = ($time % 3600);
-				} //$time >= 3600
-				if ( $time >= 60 ) {
-					$value['minutes'] = str_pad( floor( $time / 60 ), 1, 0, STR_PAD_LEFT );
-					$time             = ($time % 60);
-				} //$time >= 60
-						$value['seconds'] = str_pad( floor( $time ), 1, 0, STR_PAD_LEFT );
-					// Get the hour:minute:second version.
-				if ( '' != $value['hours'] ) {
-					$attach_hours = '<sub>' . _x( 'h', 'Hour Time', 'wp-analytify' ) . '</sub> ';
-				}
-				if ( '' != $value['minutes'] ) {
-					$attach_min = '<sub>' . _x( 'm', 'Minute Time', 'wp-analytify' ) . '</sub> ';
-				}
-				if ( '' != $value['seconds'] ) {
-					$attach_sec = '<sub>' . _x( 's', 'Second Time', 'wp-analytify' ) . '</sub>';
-				}
-						return $value['hours'] . @$attach_hours . $value['minutes'] . @$attach_min . $value['seconds'] . $attach_sec;
-					// return $value['hours'] . ':' . $value['minutes'] . ':' . $value['seconds'];
-			} //is_numeric($time)
-			else {
-				return false;
-			}
-		}
-
+ 
 		/**
 		 * Check current user role.
 		 *
